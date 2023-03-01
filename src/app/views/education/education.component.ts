fe7f68632d15c96b747091fdcd39d1bc7bff569f
Samuel-TC <samueltc39.st@gmail.com>
2023-03-01 13:01:57 -0600

    FE

--- src/app/views/education/education.component.ts
+++ src/app/views/education/education.component.ts
@@ -63,4 +63,9 @@ export class EducationComponent implements OnInit {
     window.open('file')?.document.write('<iframe src="' + '../assets/Certificate.pdf'  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
   }
 
+  
+  viewCS50(){
+    window.open('file')?.document.write('<iframe src="' + '../assets/CS50x.pdf'  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+  }
+
 }
